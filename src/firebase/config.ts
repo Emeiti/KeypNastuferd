6ffc618dd8b078e20c8436f4c8b1d55ed47d04ee
Emeiti <sunset.relay-4m@icloud.com>
2025-01-03 00:29:44 +0000

    update firebase configuration and gitignore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHvemcftiN1L_VC-xHXXQGKR2wW6en9Ro",
-  authDomain: "paddel-booking-tracker.firebaseapp.com",
-  projectId: "paddel-booking-tracker",
-  storageBucket: "paddel-booking-tracker.firebasestorage.app",
-  messagingSenderId: "290253000292",
-  appId: "1:290253000292:web:39fb11e44d988b243dd971"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
